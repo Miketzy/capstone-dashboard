6f4fc073addce8d06beab1cfdb3373a6818d0310
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2024-12-11 20:51:37 +0800

    Update Login.jsx

--- frontend-dashboard1/src/components/login/Login.jsx
+++ frontend-dashboard1/src/components/login/Login.jsx
@@ -16,7 +16,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post("http://localhost:8080/login", values);
+      const response = await axios.post("https://capstone-dashboard-d30v.onrender.com/login", values);
       if (response.data) {
         alert("Login successful!");
         localStorage.setItem("token", response.data.token);
