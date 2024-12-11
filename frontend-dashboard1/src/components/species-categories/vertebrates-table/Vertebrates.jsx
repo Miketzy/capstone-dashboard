5cacc878022277f473837234ccd4707519285197
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2024-12-11 23:14:05 +0800

    Update Vertebrates.jsx

--- frontend-dashboard1/src/components/species-categories/vertebrates-table/Vertebrates.jsx
+++ frontend-dashboard1/src/components/species-categories/vertebrates-table/Vertebrates.jsx
@@ -14,7 +14,7 @@ function Vertebrates() {
   useEffect(() => {
     // Make sure the endpoint matches your backend route
     axios
-      .get("https://capstone-dashboard-d30v.onrender.com/getvertebrates")
+      .get("https://bioexplorer-backend.onrender.com/getvertebrates")
       .then((res) => setVertibrates(res.data))
       .catch((err) => console.log(err));
   }, []);
