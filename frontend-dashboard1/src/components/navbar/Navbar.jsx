8727e52518b8bd44475fa294721b3204442e3890
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2024-12-11 23:09:20 +0800

    Update Navbar.jsx

--- frontend-dashboard1/src/components/navbar/Navbar.jsx
+++ frontend-dashboard1/src/components/navbar/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
 
   const fetchUserData = () => {
     axios
-      .get("https://capstone-dashboard-d30v.onrender.com/", { withCredentials: true })
+      .get("https://bioexplorer-backend.onrender.com/", { withCredentials: true })
       .then((response) => {
         if (response.data.message === "Profile retrieved successfully") {
           setUser({
@@ -32,7 +32,7 @@ function Navbar() {
             lastname: response.data.user.lastname || "",
             email: response.data.user.email || "",
             image: response.data.user.image
-              ? `https://capstone-dashboard-d30v.onrender.com/uploads/avatar/${response.data.user.image}`
+              ? `https://bioexplorer-backend.onrender.com/uploads/avatar/${response.data.user.image}`
               : "/images/unknown-person-icon-Image-from_20220304.png",
           });
         } else {
@@ -52,7 +52,7 @@ function Navbar() {
   const handleLogout = () => {
     if (window.confirm("Are you sure you want to log out?")) {
       axios
-        .get("https://capstone-dashboard-d30v.onrender.com/logout", { withCredentials: true })
+        .get("https://bioexplorer-backend.onrender.com/logout", { withCredentials: true })
         .then((res) => {
           if (res.data.Message === "Success") {
             setUser({
