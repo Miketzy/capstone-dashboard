283241588737cadda2b5790bd171d69672603713
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2024-12-11 21:02:18 +0800

    Update Reptile.jsx

--- frontend-dashboard1/src/components/species-categories/reptiles-table/Reptile.jsx
+++ frontend-dashboard1/src/components/species-categories/reptiles-table/Reptile.jsx
@@ -13,7 +13,7 @@ function Reptile() {
 
   useEffect(() => {
     axios
-      .get("http://localhost:8080/getReptiles") // Updated endpoint
+      .get("https://capstone-dashboard-d30v.onrender.com/getReptiles") // Updated endpoint
       .then((res) => setReptiles(res.data))
       .catch((err) => console.log(err));
   }, []);
