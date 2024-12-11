2bcc215b713469a2d667af3dcdac71abd2d8ef42
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2024-12-11 22:56:52 +0800

    Update EditProfile.jsx

--- frontend-dashboard1/src/components/Button-Profile/Edit-Profile/EditProfile.jsx
+++ frontend-dashboard1/src/components/Button-Profile/Edit-Profile/EditProfile.jsx
@@ -34,13 +34,13 @@ function EditProfile({ onUpdateProfile }) {
     // Fetch user data from backend
     const fetchUserData = async () => {
       try {
-        const res = await axios.get("https://capstone-dashboard-d30v.onrender.com/myprofile", {
+        const res = await axios.get("https://bioexplorer-backend.onrender.com/myprofile", {
           withCredentials: true,
         });
 
         const user = res.data.user;
         const imageUrl = user.image
-          ? `https://capstone-dashboard-d30v.onrender.com/uploads/avatar/${user.image}`
+          ? `https://bioexplorer-backend.onrender.com/uploads/avatar/${user.image}`
           : "/images/unknown-person-icon-Image-from_20220304.png";
 
         // Set user data including the image URL
@@ -90,7 +90,7 @@ function EditProfile({ onUpdateProfile }) {
     }
 
     try {
-      const res = await axios.put("https://capstone-dashboard-d30v.onrender.com/profile", formData, {
+      const res = await axios.put("https://bioexplorer-backend.onrender.com/profile", formData, {
         headers: {
           Authorization: `Bearer ${token}`,
           "Content-Type": "multipart/form-data",
@@ -103,7 +103,7 @@ function EditProfile({ onUpdateProfile }) {
         setUserData(res.data);
         setSelectedImage(
           res.data.image
-            ? `https://capstone-dashboard-d30v.onrender.com/uploads/avatar/${res.data.image}`
+            ? `https://bioexplorer-backend.onrender.com/uploads/avatar/${res.data.image}`
             : "/images/unknown-person-icon-Image-from_20220304.png"
         );
 
