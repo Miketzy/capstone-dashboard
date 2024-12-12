17c0366ef8ed9b1710bef0843713f5329abbfdfb
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2024-12-12 16:45:32 +0800

    commit

--- backend-dashboard1/server.js
+++ backend-dashboard1/server.js
@@ -34,7 +34,7 @@ app.use(cors({
 
 
 const connection = mysql2.createConnection({
-  host: ' sql5.freesqldatabase.com',    // The server address
+  host: '121.54.70.157',    // The server address
   user: 'sql5751289',                   // Your username
   password: 'QpWkamYpQC',               // Your password
   database: 'sql5751289',               // Your database name
