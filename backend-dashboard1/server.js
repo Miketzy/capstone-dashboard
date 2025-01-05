45d1c93f9023c664eca50fe1d3783118165352d7
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-01-05 10:11:26 +0800

    commit24

--- backend-dashboard1/server.js
+++ backend-dashboard1/server.js
@@ -1165,7 +1165,8 @@ app.delete('/delete-species/:id', (req, res) => {
 });
 
 // Middleware to serve static files
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static('uploads'));  
+
 // Route to fetch images
 app.get('/api/images', (req, res) => {
   const sql = 'SELECT id, commonname, uploadimage FROM species';
