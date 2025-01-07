8ea29fff0bda02210fb5e3f0c4111263aa08ea05
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-01-07 14:19:55 +0800

    changes5

--- backend-dashboard1/server.js
+++ backend-dashboard1/server.js
@@ -34,7 +34,7 @@ const server = http.createServer(app);
 
 
 app.use(cors({
-  origin: "https://bio-explorer-admin.onrender.com",
+  origin: "https://davao-oreintal-bioexplorer-admin-lsxrdhin8.vercel.app",
   credentials: true,  // Allow cookies, if needed
 }));
 
