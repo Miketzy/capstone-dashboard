7e2e440827dfa960755208c6c45a7c74e902a9a4
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-22 11:57:30 +0800

    updated

--- backend-dashboard1/src/config/db.js
+++ backend-dashboard1/src/config/db.js
@@ -1,8 +1,9 @@
-import { Pool } from 'pg';
+import pkg from 'pg';
+
 import dotenv from 'dotenv';
 
 dotenv.config();  // Load .env variables
-
+const { Pool } = pkg;
 const pool = new Pool({
   connectionString: "postgresql://reposatory:71YyyVsRMV2544ho7UjtQcGw3UcHXUSg@dpg-cvemttd2ng1s73ci1oag-a.oregon-postgres.render.com/reposatory",
   ssl: { rejectUnauthorized: false }, // Required for cloud databases like Render
