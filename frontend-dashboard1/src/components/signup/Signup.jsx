a31a94eb1ecebdbe2b3cb3497e346e2d76ee8f77
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-22 13:19:38 +0800

    updated

--- frontend-dashboard1/src/components/signup/Signup.jsx
+++ frontend-dashboard1/src/components/signup/Signup.jsx
@@ -14,7 +14,7 @@ function Register() {
     password: "",
     confirmPassword: "",
   });
-  const apiUrl = process.env.REACT_APP_BACKEND_URL;
+  const apiUrl = process.env.REACT_APP_API_URL;
   const navigate = useNavigate();
 
   const handleChange = (e) => {
