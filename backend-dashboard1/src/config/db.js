b02bf55383d5087fd976e5f68e88769a494a442b
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-22 10:12:25 +0800

    updated

--- backend-dashboard1/src/config/db.js
+++ backend-dashboard1/src/config/db.js
@@ -1,18 +1,19 @@
-import { Client } from "pg";
+import { Pool } from "pg";
 import dotenv from "dotenv";
 
-dotenv.config(); // Load environment variables from .env
+dotenv.config();
 
-const client = new Client({
+const pool = new Pool({
   host: process.env.PG_HOST,
   user: process.env.PG_USER,
   password: process.env.PG_PASSWORD,
   database: process.env.PG_DATABASE,
   port: process.env.PG_PORT,
+  ssl: { rejectUnauthorized: false }, // Important sa Render
 });
 
-client.connect()
+pool.connect()
   .then(() => console.log("✅ Connected to PostgreSQL Database"))
   .catch((err) => console.error("❌ Database Connection Failed:", err));
 
-export default client;
+export default pool;
