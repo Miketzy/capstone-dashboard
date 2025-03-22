e1bbcbd174f9dc246cec78a46c5d1505bbb12085
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-22 11:46:47 +0800

    updated

--- backend-dashboard1/src/config/db.js
+++ backend-dashboard1/src/config/db.js
@@ -1,11 +1,13 @@
-import { Pool } from "pg";  // Directly import Pool from pg
+import pkg from "pg";
 import dotenv from "dotenv";
 
-dotenv.config();  // Load environment variables from .env file
+dotenv.config();
+
+const { Pool } = pkg;
 
 const pool = new Pool({
-  connectionString: process.env.PG_HOST,  // Using the full connection string from the .env file
-  ssl: { rejectUnauthorized: false },  // For Render Database SSL configuration
+    connectionString: process.env.PG_HOST,  // Using the full connection string from the .env file
+  ssl: { rejectUnauthorized: false }, // Para sa Render
 });
 
 // Test database connection
@@ -13,4 +15,4 @@ pool.query("SELECT 1")
   .then(() => console.log("✅ Connected to PostgreSQL Database"))
   .catch((err) => console.error("❌ Database Connection Failed:", err));
 
-export default pool;  // Export pool for use in other parts of your application
+  export default pool;  // ✅ Dapat "export default pool"
