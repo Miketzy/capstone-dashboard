0811408ee67c67b42ad57b8a483ae8676a6b7f49
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-22 12:58:55 +0800

    updated

--- frontend-dashboard1/src/components/signup/Signup.jsx
+++ frontend-dashboard1/src/components/signup/Signup.jsx
@@ -34,7 +34,7 @@ function Register() {
 
     const updatedFormData = { ...formData, phone_number: formattedPhoneNumber };
 
-    fetch(`${apiUrl}/api-register`, {
+    fetch(`${apiUrl}/api-register/register`, {
       method: "POST",
       body: JSON.stringify(updatedFormData),
       headers: {
