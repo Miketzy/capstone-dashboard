1a78aa6fc38593c2a54a1eb07218c7e03cdaf553
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-22 11:54:30 +0800

    updated

--- backend-dashboard1/src/config/db.js
+++ backend-dashboard1/src/config/db.js
@@ -1,18 +1,11 @@
-import pkg from "pg";
-import dotenv from "dotenv";
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
 
-dotenv.config();
-
-const { Pool } = pkg;
+dotenv.config();  // Load .env variables
 
 const pool = new Pool({
-    connectionString: process.env.PG_HOST,  // Using the full connection string from the .env file
-  ssl: { rejectUnauthorized: false }, // Para sa Render
+  connectionString: "postgresql://reposatory:71YyyVsRMV2544ho7UjtQcGw3UcHXUSg@dpg-cvemttd2ng1s73ci1oag-a.oregon-postgres.render.com/reposatory",
+  ssl: { rejectUnauthorized: false }, // Required for cloud databases like Render
 });
 
-// Test database connection
-pool.query("SELECT 1")
-  .then(() => console.log("✅ Connected to PostgreSQL Database"))
-  .catch((err) => console.error("❌ Database Connection Failed:", err));
-
-  export default pool;  // ✅ Dapat "export default pool"
+export default pool;
