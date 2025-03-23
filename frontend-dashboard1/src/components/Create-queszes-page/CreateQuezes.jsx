998ee7d08273c5426a6dddfbcd1193d298ba0ebe
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-23 14:06:08 +0800

    updated

--- frontend-dashboard1/src/components/Create-queszes-page/CreateQuezes.jsx
+++ frontend-dashboard1/src/components/Create-queszes-page/CreateQuezes.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./CreateQuizzes.css";
+import "./CreateQuezes.css";
 
 function CreateQuizzes() {
   const [question, setQuestion] = useState("");
