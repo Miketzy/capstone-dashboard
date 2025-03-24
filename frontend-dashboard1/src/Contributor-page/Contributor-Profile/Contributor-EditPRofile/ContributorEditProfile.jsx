a84fcc052cafc3bccc5ce6b68e9ac42b1481ba93
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-24 19:16:08 +0800

    updated

--- frontend-dashboard1/src/Contributor-page/Contributor-Profile/Contributor-EditPRofile/ContributorEditProfile.jsx
+++ frontend-dashboard1/src/Contributor-page/Contributor-Profile/Contributor-EditPRofile/ContributorEditProfile.jsx
@@ -1,7 +1,12 @@
 import React, { useState, useEffect } from "react";
+import { BsPlusCircleDotted } from "react-icons/bs";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const EditProfile = () => {
+function ContributorEditProfile({ onUpdateProfile }) {
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [imageFile, setImageFile] = useState(null);
+  const [loading, setLoading] = useState(true);
   const [userData, setUserData] = useState({
     firstname: "",
     middlename: "",
@@ -10,54 +15,53 @@ const EditProfile = () => {
     gender: "",
     phone_number: "",
     username: "",
-    image: null,
+    image: "/images/unknown-person-icon-Image-from_20220304.png",
   });
+  const navigate = useNavigate();
+
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      setSelectedImage(URL.createObjectURL(file));
+      setImageFile(file);
+    }
+  };
 
   useEffect(() => {
     const fetchUserData = async () => {
       try {
-        const token = localStorage.getItem("token");
         const res = await axios.get(
-          "https://bioexplorer-backend.onrender.com/profile",
+          "https://bioexplorer-backend.onrender.com/myprofile",
           {
-            headers: { Authorization: `Bearer ${token}` },
+            withCredentials: true,
           }
         );
-
-        if (res.data) {
-          setUserData({
-            firstname: res.data.firstname || "",
-            middlename: res.data.middlename || "",
-            lastname: res.data.lastname || "",
-            email: res.data.email || "",
-            gender: res.data.gender || "",
-            phone_number: res.data.phone_number || "",
-            username: res.data.username || "", // Ensure username is set
-            image: res.data.image || null,
-          });
+        const user = res.data.user;
+        const imageUrl = user.image
+          ? `https://bioexplorer-backend.onrender.com/uploads/avatar/${user.image}`
+          : "/images/unknown-person-icon-Image-from_20220304.png";
+        setUserData({
+          ...userData,
+          firstname: user.firstname || "",
+          middlename: user.middlename || "",
+          lastname: user.lastname || "",
+          email: user.email || "",
+          gender: user.gender || "",
+          phone_number: user.phone_number || "",
+          username: user.username || "",
+          image: imageUrl,
+        });
+      } catch (err) {
+        console.error("Error fetching profile:", err);
+        if (err.response && err.response.status === 401) {
+          navigate("/");
         }
-      } catch (error) {
-        console.error("Error fetching profile:", error);
+      } finally {
+        setLoading(false);
       }
     };
-
     fetchUserData();
-  }, []);
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-  };
-
-  const handleFileChange = (e) => {
-    setUserData((prevData) => ({
-      ...prevData,
-      image: e.target.files[0],
-    }));
-  };
+  }, [navigate]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -100,68 +104,153 @@ const EditProfile = () => {
   };
 
   return (
-    <div>
-      <h2>Edit Profile</h2>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          name="firstname"
-          value={userData.firstname}
-          onChange={handleChange}
-          placeholder="First Name"
-          required
-        />
-        <input
-          type="text"
-          name="middlename"
-          value={userData.middlename}
-          onChange={handleChange}
-          placeholder="Middle Name"
-        />
-        <input
-          type="text"
-          name="lastname"
-          value={userData.lastname}
-          onChange={handleChange}
-          placeholder="Last Name"
-          required
-        />
-        <input
-          type="email"
-          name="email"
-          value={userData.email}
-          onChange={handleChange}
-          placeholder="Email"
-          required
-        />
-        <input
-          type="text"
-          name="gender"
-          value={userData.gender}
-          onChange={handleChange}
-          placeholder="Gender"
-          required
-        />
-        <input
-          type="text"
-          name="phone_number"
-          value={userData.phone_number}
-          onChange={handleChange}
-          placeholder="Phone Number"
-          required
-        />
-        <input
-          type="text"
-          name="username"
-          value={userData.username}
-          readOnly
-          placeholder="Username (Cannot be changed)"
-        />
-        <input type="file" onChange={handleFileChange} />
-        <button type="submit">Update Profile</button>
-      </form>
+    <div className=" flex justify-center items-center py-10 px-4 sm:px-6 lg:px-8 min-h-screen">
+      <div className="bg-white shadow-md rounded-lg w-full max-w-4xl p-6">
+        <div className="flex justify-center">
+          <div className="relative w-32 h-32 rounded-full overflow-hidden bg-gray-200 shadow">
+            <img
+              src={selectedImage || userData.image}
+              alt="Profile"
+              className="w-full h-full object-cover"
+            />
+            <label
+              htmlFor="profile-imgInput"
+              className="absolute inset-0 flex justify-center items-center"
+            >
+              <input
+                type="file"
+                id="profile-imgInput"
+                accept="image/*"
+                className="hidden"
+                onChange={handleImageChange}
+              />
+              <div className="bg-blue-500 p-2 rounded-full text-white cursor-pointer mt-20">
+                <BsPlusCircleDotted size={20} />
+              </div>
+            </label>
+          </div>
+        </div>
+
+        <form className="mt-6 space-y-6" onSubmit={handleSubmit}>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Firstname
+              </label>
+              <input
+                type="text"
+                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                placeholder="Enter your Firstname"
+                value={userData.firstname}
+                onChange={(e) =>
+                  setUserData({ ...userData, firstname: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Middlename
+              </label>
+              <input
+                type="text"
+                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                placeholder="Enter your Middlename"
+                value={userData.middlename}
+                onChange={(e) =>
+                  setUserData({ ...userData, middlename: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Lastname
+              </label>
+              <input
+                type="text"
+                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                placeholder="Enter your Lastname"
+                value={userData.lastname}
+                onChange={(e) =>
+                  setUserData({ ...userData, lastname: e.target.value })
+                }
+              />
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Email
+              </label>
+              <input
+                type="email"
+                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                placeholder="Enter your Email"
+                value={userData.email}
+                onChange={(e) =>
+                  setUserData({ ...userData, email: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Gender
+              </label>
+              <select
+                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                value={userData.gender}
+                onChange={(e) =>
+                  setUserData({ ...userData, gender: e.target.value })
+                }
+              >
+                <option value="">Select Gender</option>
+                <option value="Male">Male</option>
+                <option value="Female">Female</option>
+                <option value="Other">Other</option>
+              </select>
+            </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700">
+                Phone Number
+              </label>
+              <input
+                type="tel"
+                className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+                placeholder="Enter your Phone number"
+                value={userData.phone_number}
+                onChange={(e) =>
+                  setUserData({ ...userData, phone_number: e.target.value })
+                }
+              />
+            </div>
+          </div>
+          <div>
+            <label className="block text-sm font-medium text-gray-700">
+              Username
+            </label>
+            <input
+              type="text"
+              className="mt-1 block w-full border border-gray-300 rounded-md shadow-sm p-2"
+              placeholder="Enter Username"
+              value={userData.username}
+              onChange={(e) =>
+                setUserData({ ...userData, username: e.target.value })
+              }
+            />
+          </div>
+          <div className="flex justify-center mt-4">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded shadow w-full sm:w-auto"
+            >
+              Save Changes
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
-};
+}
 
-export default EditProfile;
+export default ContributorEditProfile;
