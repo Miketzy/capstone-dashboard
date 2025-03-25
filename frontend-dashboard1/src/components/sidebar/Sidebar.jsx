3664af0a1da2cb7bf34d758f58e163e748659f43
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-25 20:37:40 +0800

    updated

--- frontend-dashboard1/src/components/sidebar/Sidebar.jsx
+++ frontend-dashboard1/src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import "./sidebar.css";
 import Button from "@mui/material/Button";
 import { RxDashboard } from "react-icons/rx";
 import { FaAngleRight } from "react-icons/fa";
@@ -47,113 +46,17 @@ function Sidebar() {
   };
 
   return (
-    <div className="sidebar">
-      <div className="top">
-        <div className="sidebarTitle"></div>
-        <div className="close" id="close-btn">
-          <span className="material-icon-sharp">
-            <IoCloseSharp />
-          </span>
-        </div>
-      </div>
-
-      <ul>
-        <li>
-          <Button
-            className={`w-100 ${
-              activeTab === "/species-directory" ? "active" : ""
-            }`}
-            onClick={() => handleNavigation("/species-directory")}
-          >
-            <span className="icon">
-              <RxDashboard />
+    <div className="w-64 bg-gray-800 fixed h-full ">
+      <div>
+        <ul>
+          <li>
+            <span>
+              <RxDashboard className="inline-block w-6 h-6 mr-2 -mt2" />
             </span>
             Species Directory
-          </Button>
-        </li>
-
-        <li>
-          <Button
-            className={`w-100 ${activeTab === "/add-species" ? "active" : ""}`}
-            onClick={() => handleNavigation("/add-species")}
-          >
-            <span className="icon">
-              <IoMdAddCircle />
-            </span>
-            Add Species
-          </Button>
-        </li>
-
-        <li>
-          <Button
-            className={`w-100 ${
-              activeTab === "/list-of-species" ? "active" : ""
-            }`}
-            onClick={() => handleNavigation("/list-of-species")}
-          >
-            <span className="icon">
-              <MdEditCalendar />
-            </span>
-            List of Species
-          </Button>
-        </li>
-
-        <li>
-          <Button
-            className={`w-100 ${
-              activeTab === "/species-analytics" ? "active" : ""
-            }`}
-            onClick={() => handleNavigation("/species-analytics")}
-          >
-            <span className="icon">
-              <MdAnalytics />
-            </span>
-            Analytics
-          </Button>
-        </li>
-        <li>
-          <Button
-            className={`w-100 ${
-              activeTab === "/createQuestion" ? "active" : ""
-            }`}
-            onClick={() => handleNavigation("/createQuestion")}
-          >
-            <span className="icon">
-              <FaFileCircleQuestion />
-            </span>
-            Create Question
-          </Button>
-        </li>
-
-        <li>
-          <Button
-            className={`w-100 ${
-              activeTab === "/image-gallery" ? "active" : ""
-            }`}
-            onClick={() => handleNavigation("/image-gallery")}
-          >
-            <span className="icon">
-              <GrGallery />
-            </span>
-            Gallery
-          </Button>
-        </li>
-
-        <li>
-          <Button
-            className={`w-100 ${
-              activeTab === "/contributor-request" ? "active" : ""
-            }`}
-            onClick={() => handleNavigation("/contributor-request")}
-          >
-            <span className="icon">
-              <IoPeopleOutline />
-            </span>
-            Contributor Request
-            {requestCount > 0 && <span className="badge">{requestCount}</span>}
-          </Button>
-        </li>
-      </ul>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
