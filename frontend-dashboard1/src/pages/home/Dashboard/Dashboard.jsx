b055cd61ebecb2d869e0b01482c1f652fa884f2a
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-25 19:49:24 +0800

    updated

--- frontend-dashboard1/src/pages/home/Dashboard/Dashboard.jsx
+++ frontend-dashboard1/src/pages/home/Dashboard/Dashboard.jsx
@@ -4,27 +4,22 @@ import SpeciesDirectoryHome from "../../PagesHome/species-directory-home/Species
 
 function Dashboard() {
   return (
-    <>
-      <div>
-        <div className=" main ">
-          <Navbar />
-          <div className="flex">
-            <div className="">
-              <Sidebar />
-            </div>
-
-            <div className="content">
-              <div className="tit">
-                <h1 className="tit1">Species Directory</h1>
-              </div>
-              <div className="speciestable">
-                <SpeciesDirectoryHome />
-              </div>
-            </div>
+    <div className="min-h-screen flex flex-col bg-gray-100">
+      <Navbar />
+      <div className="flex flex-grow">
+        <Sidebar />
+        <div className="flex flex-col flex-grow p-6">
+          <div className="mb-6 text-center">
+            <h1 className="text-3xl font-bold text-gray-800">
+              Species Directory
+            </h1>
+          </div>
+          <div className="bg-white shadow-lg rounded-lg p-4">
+            <SpeciesDirectoryHome />
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
 
