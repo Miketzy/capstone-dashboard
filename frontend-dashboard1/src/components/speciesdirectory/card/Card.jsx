c274251b4ebee4f5bbbcda37af0863ef54fa5388
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-26 08:49:51 +0800

    updated

--- frontend-dashboard1/src/components/speciesdirectory/card/Card.jsx
+++ frontend-dashboard1/src/components/speciesdirectory/card/Card.jsx
@@ -125,7 +125,7 @@ function Card() {
       </h2>
 
       {/* Responsive Grid Layout */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-1">
         {categories.map((category, index) => (
           <div
             key={index}
