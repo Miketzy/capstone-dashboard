8adb4dc71945575e545fafc27a33586d580e3ba7
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-27 05:47:25 +0800

    updated

--- frontend-dashboard1/src/components/speciesdirectory/card/Card.jsx
+++ frontend-dashboard1/src/components/speciesdirectory/card/Card.jsx
@@ -146,7 +146,7 @@ function Card() {
 
       {/* Grid Layout for Cards */}
       <div className="flex flex-col gap-y-4">
-        <div className="grid grid-cols-1 gap-4 grid-colssm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  ">
+        <div className="grid grid-cols-1 gap-4 grid-colssm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3  ">
           {(activeTab === "vertebrates" ? vertebrates : invertebrates).map(
             (category, index) => (
               <div
