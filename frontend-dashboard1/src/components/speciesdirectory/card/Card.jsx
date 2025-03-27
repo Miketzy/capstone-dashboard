f18df1ba22b35b8c2d347ff34db3ff3fd5357c62
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-27 11:00:00 +0800

    updated

--- frontend-dashboard1/src/components/speciesdirectory/card/Card.jsx
+++ frontend-dashboard1/src/components/speciesdirectory/card/Card.jsx
@@ -142,7 +142,7 @@ function Card() {
           Invertebrates
         </button>
       </div>
-      <div className="grid grid-cols-3 gap-x-3 gap-y-3">
+      <div className="grid grid-cols-3 gap-x-3 gap-y-3 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
         {(activeTab === "vertebrates" ? vertebrates : invertebrates).map(
           (category, index) => (
             <div
