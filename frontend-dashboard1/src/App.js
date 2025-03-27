ebf910efe68bee0dceeeaef7e238fe5478b1dcfd
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-28 05:42:17 +0800

    updated

--- frontend-dashboard1/src/App.js
+++ frontend-dashboard1/src/App.js
@@ -5,18 +5,6 @@ import Signup from './components/signup/Signup';
 import Dashboard from './pages/home/Dashboard/Dashboard';
 import AddSpeciesDashboard from "./pages/home/Add-species-Dashboard/AddSpeciesDashboard";
 import ViewAndEditDashboard from "./pages/home/View-Edit-Species-Dashboard/ViewAndEditDashboard";
-import MammalsTable from "./pages/home/species--categories-dashboard/mammalstable-dashboard/MammalsTable";
-import BirdsTableDashboard from "./pages/home/species--categories-dashboard/birdstable-dashboard/BirdsTableDashboard";
-import ReptilesTableDashboard from "./pages/home/species--categories-dashboard/reptilestable-dashboard/ReptilesTableDashboard";
-import AmphibianstableDashboard from "./pages/home/species--categories-dashboard/amphibianstable-dashboard/AmphibianstableDashboard";
-import InvertebratestableDashboard from "./pages/home/species--categories-dashboard/invertebratestable-dashboard/InvertebratestableDashboard";
-import FishtablesDashboard from "./pages/home/species--categories-dashboard/fishtable-dashboard/FishtablesDashboard";
-import SpeciesAnalyticsDasboard from "./pages/home/Species-Analytics-Dashboard/SpeciesAnalyticsDasboard";
-import CriticalEndengeredDashboard from "./pages/home/Conservation-Status-Home/Critical-Endengered-Dashboard/CriticalEndengeredDashboard";
-import EndengeredDashboard from "./pages/home/Conservation-Status-Home/Endengered-Dashboard/EndengeredDashboard";
-import VulnerableDashboard from "./pages/home/Conservation-Status-Home/Vulnerable-Dashboard/VulnerableDashboard";
-import NearThreatendDashboard from "./pages/home/Conservation-Status-Home/Near-Threatend-Dashboard/NearThreatendDashboard";
-import LeastConcernDashboard from "./pages/home/Conservation-Status-Home/Least-Concern-Dashboard/LeastConcernDashboard";
 import MyProfileDashboard from "./pages/home/My-Profile-Dashboard/MyProfileDashboard";
 import EditProfileDashboard from "./pages/home/Button-Profile-Dashboard/Edit-Profile-Dashboard/EditProfileDashboard";
 import ChangePasswordDashboard from "./pages/home/Button-Profile-Dashboard/Change-Password-Dashboard/ChangePasswordDashboard";
@@ -32,7 +20,6 @@ import ContributorEditProfileDashboard from "./pages/Contributor-Home/Contributo
 import ContributorChangePasswordDashboard from "./pages/Contributor-Home/Contributor-Profile-Dashboard/Contributor-ChangePassword-Dashboard/ContributorChangePasswordDashboard";
 import ContributorRequestDashboards from "./pages/home/Contributor-Request-Dashboard/ContributorRequestDashboards";
 import CreateQuizesDashboard from "./pages/home/Create-Quizes-Dashboard/CreateQuizesDashboard";
-import VertebratesTableDashboard from "./pages/home/species--categories-dashboard/vertebrates-dashboard/VertebratesTableDashboard";
 import ContributorMyprofileDashboard from "./pages/Contributor-Home/Contributor-Profile-Dashboard/ContributotMyProfileDashboard/ContributorMyprofileDashboard";
 import CheckAuth from "./checkAuth/CheckAuth";
 import { useEffect } from 'react';
@@ -72,19 +59,7 @@ function App() {
         <Route path='/contributor-request' element={<ContributorRequestDashboards/>} />
         <Route path='/list-of-species' element={<ViewAndEditDashboard/>} />
         <Route path='/createQuestion' element={<CreateQuizesDashboard/>} />
-        <Route path='/species-categories/mammals' element={<MammalsTable/>} />
-        <Route path='/species-categories/birds' element={<BirdsTableDashboard/>} />
-        <Route path='/species-categories/reptiles' element={<ReptilesTableDashboard/>} />
-        <Route path='/species-categories/amphibians' element={<AmphibianstableDashboard/>} />
-        <Route path='/species-categories/invertebrates' element={<InvertebratestableDashboard/>} />
-        <Route path='/species-categories/vertebrates' element={<VertebratesTableDashboard/>} />
-         <Route path='/species-categories/fish' element={<FishtablesDashboard/>} />
         <Route path='/species-analytics' element={<SpeciesAnalyticsDasboard/>} />
-        <Route path='/conservation-status/critical-endengered' element={<CriticalEndengeredDashboard/>} />
-        <Route path='/conservation-status/endengered' element={<EndengeredDashboard/>} />
-        <Route path='/conservation-status/vulnerable' element={<VulnerableDashboard/>} />
-        <Route path='/conservation-status/near-threatend' element={<NearThreatendDashboard/>} />
-        <Route path='/conservation-status/least-concern' element={<LeastConcernDashboard/>} />
         <Route path='/my-profile' element={<MyProfileDashboard/>} />
         <Route path='/my-profile/edit-profile' element={<EditProfileDashboard/>} />
         <Route path='/my-profile/change-password' element={<ChangePasswordDashboard/>} />
