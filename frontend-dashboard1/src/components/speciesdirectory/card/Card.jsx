70303a519fe796bdc650f41068450c125cc0359b
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-27 20:35:14 +0800

    updated

--- frontend-dashboard1/src/components/speciesdirectory/card/Card.jsx
+++ frontend-dashboard1/src/components/speciesdirectory/card/Card.jsx
@@ -161,9 +161,6 @@ function Card() {
 
   return (
     <div>
-      <div className="bg-white shadow-lg rounded-lg p-6 mt-50">
-        <h1 className="text-3xl font-bold text-center ">Dashboard</h1>
-      </div>
       <div className="flex justify-center mb-6">
         <button
           className={`px-6 py-2 mx-2 rounded-lg text-white ${
