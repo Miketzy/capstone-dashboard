3bc88c7a019cfc51788e34ff266ab7c125e6fad3
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-03-28 15:21:19 +0800

    updated

--- backend-dashboard1/src/routes/LoginRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { loginUser } from "../controller/LoginController.js"; // ✅ Tama ang path
-
-const router = express.Router();
-
-router.post("/login", loginUser);
-
-export default router;
--- backend-dashboard1/src/routes/RegistrationRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { registerUser } from "../controller/RegisterController.js";
-
-const router = express.Router();
-
-router.post("/register", registerUser);
-
-export default router;
