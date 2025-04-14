abc4b7883a68a3347a8a454d3f5277a787586bf3
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-04-14 18:23:07 +0800

    updated

--- frontend-dashboard1/src/components/species-add-date/SpeciesMonth.jsx
+++ frontend-dashboard1/src/components/species-add-date/SpeciesMonth.jsx
@@ -77,7 +77,7 @@ const SpeciesMonth = () => {
   }, []);
 
   return (
-    <div className="container mx-auto px-4 py-8 mt-[-100px] sm:mt-[-150px] md:mt-[-200px] lg:mt-[-250px] xl:mt-[-350px] 2xl:mt-[-350px]">
+    <div className="container mx-auto px-4 py-8 mt-[-100px] sm:mt-[-200px] md:mt-[-300px] lg:mt-[-350px] xl:mt-[-350px] 2xl:mt-[-350px]">
       <div
         style={{
           backgroundColor: "white",
