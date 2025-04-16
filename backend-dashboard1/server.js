7b37c372b33a565de82583d508b7bf52ca25e9a3
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-04-16 09:28:37 +0800

    updated

--- backend-dashboard1/server.js
+++ backend-dashboard1/server.js
@@ -1735,6 +1735,22 @@ app.get("/api/species/monthly", async (req, res) => {
       COUNT(*) AS count
     FROM species
     GROUP BY created_month
+    ORDER BY 
+      CASE created_month
+        WHEN 'January' THEN 1
+        WHEN 'February' THEN 2
+        WHEN 'March' THEN 3
+        WHEN 'April' THEN 4
+        WHEN 'May' THEN 5
+        WHEN 'June' THEN 6
+        WHEN 'July' THEN 7
+        WHEN 'August' THEN 8
+        WHEN 'September' THEN 9
+        WHEN 'October' THEN 10
+        WHEN 'November' THEN 11
+        WHEN 'December' THEN 12
+        ELSE 13
+      END
   `;
 
   try {
