c08a12f166a17dc404dc66eff9a957308aa5d06e
Michael John G. Margate <101804266+Miketzy@users.noreply.github.com>
2025-05-17 18:38:41 +0800

    updated

--- backend-dashboard1/server.js
+++ backend-dashboard1/server.js
@@ -1222,7 +1222,7 @@ app.put("/species/approve/:id", async (req, res) => {
     const insertSql = `
       INSERT INTO species (
         specificname, scientificname, commonname, habitat, population, threats, 
-        location, conservationstatus, speciescategory, conservationeffort, description, 
+        location, conservationstatus, speciescategory, conservationeffort, description,classification, 
         uploadimage, uploadimage1, uploadimage2, uploadimage3, created_at, created_month, latitude, longitude
       )
       VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19, $20 )
